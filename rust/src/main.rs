43493356879111dbf93b273f50d179ef8baa10ba
ALX <115327608+N0T-THE-F0X@users.noreply.github.com>
2022-12-13 21:19:36 -0700

    Still dose the same thing but it looks better

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,34 +1,41 @@
+//              _       __   __ 
+//     //\     | |      \ \ / / 
+//    //_\\    | |       \ / /  
+//   //___\\   | |___    / / \  
+//  //     \\  |_____|  /_/ \_\ 
+
+// Crates
+use std::io;
 
 
 fn main() {
-// Outline of the project
+// Outline of the project check the respective function for more info
+    print_alx();
+    password_check();
+    
+}
 
+// Functions
+
+fn print_alx(){
+    //prints out my logo
     println!(r"              _       __   __ ");
     println!(r"     //\     | |      \ \ / / ");
     println!(r"    //_\\    | |       \ / /  ");
     println!(r"   //___\\   | |___    / / \  ");
     println!(r"  //     \\  |_____|  /_/ \_\ ");
-
-    password_check();    
-
+    println!(r"                              ");
 }
-
-// My comands
-
 fn password_check(){
-    
+    //checks password
     let mut line = String::new();
         println!("PASSWORD:");
         std::io::stdin().read_line(&mut line ).unwrap();
 
         if line == "Joshua"{
             println!("HELLO PROFESSOR");
-
         }
-        else {
+        if line != "Joshua" {
             println!("PASSWORD: DENIED");
-        
         }
 }
-
-
